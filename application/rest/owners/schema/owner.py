0885db5da00d73690b35962864aefe47b40c2a86
jackteruya <jack.teruya@gmail.com>
2023-02-14 07:41:06 -0400

    update owner

--- application/rest/owners/schema/owner.py
+++ application/rest/owners/schema/owner.py
@@ -6,17 +6,3 @@ class OwnerCreateRequest(Schema):
     name = fields.String(required=True)
     document = fields.String(required=True)
     date_of_birth = fields.Date(required=True)
-
-
-resource_fields = Model('Owner', {
-    'name': fields.String,
-    'document': fields.String,
-    'date_of_birth': fields.Date,
-})
-
-response_fields = Model('Owner Response', {
-    'id': fields.Integer,
-    'name': fields.String,
-    'document': fields.String,
-    'date_of_birth': fields.Date,
-})
