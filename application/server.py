c47543bb484f60cab01946ce76be6b5f450b4afb
jackteruya <jack.teruya@gmail.com>
2023-02-14 07:42:31 -0400

    updated server

--- application/server.py
+++ application/server.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 from flask_restx import Api
 from werkzeug.middleware.proxy_fix import ProxyFix
 
+from application.rest.cars.car_create import api_car_create
+from application.rest.cars.car_get import api_car_get
 from application.rest.owners.owner_create import api_owner_create
 from application.rest.owners.owner_get import api_owner_get
 
@@ -23,6 +25,8 @@ class ServeApplication:
 
         api.add_namespace(api_owner_get)
         api.add_namespace(api_owner_create)
+        api.add_namespace(api_car_get)
+        api.add_namespace(api_car_create)
         app.register_blueprint(self._blueprint)
 
     def create_app(self):
